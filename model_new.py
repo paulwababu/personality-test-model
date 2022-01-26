1e3cdb234a31b95f36b019b64a66187d84ee7ddc
root <root@instance-1.us-central1-a.c.loyal-surfer-320617.internal>
2022-01-26 18:49:12 +0000

    built new model

--- model_new.py
+++ model_new.py
@@ -32,4 +32,74 @@ plt.figure(figsize=(15,5))
 sns.barplot(data=countries_5000, x=countries_5000.index, y='country')
 plt.title('Countries With More Than 5000 Participants')
 plt.ylabel('Participants');
-plt.savefig('save_as_a_png.png')
+#plt.savefig('save_as_a_png.png')
+
+df = data.drop('country', axis=1)
+columns = list(df.columns)
+
+scaler = MinMaxScaler(feature_range=(0,1))
+df = scaler.fit_transform(df)
+df = pd.DataFrame(df, columns=columns)
+df_sample = df[:1013481]
+
+kmeans = KMeans()
+visualizer = KElbowVisualizer(kmeans, k=(2,15))
+visualizer.fit(df_sample)
+visualizer.poof()
+
+df_model = data.drop('country', axis=1)
+
+# I define 5 clusters and fit my model
+kmeans = KMeans(n_clusters=5)
+k_fit = kmeans.fit(df_model)
+## SAVE KFIT MODEL
+joblib_file = "BigFivePersonalityTestModel(2).joblib"
+joblib.dump(k_fit, joblib_file)
+
+# Predicting the Clusters
+pd.options.display.max_columns = 10
+predictions = k_fit.labels_
+df_model['Clusters'] = predictions
+df_model.head()
+
+pd.options.display.max_columns = 150
+df_model.groupby('Clusters').mean()
+
+# Summing up the different questions groups
+col_list = list(df_model)
+ext = col_list[0:10]
+est = col_list[10:20]
+agr = col_list[20:30]
+csn = col_list[30:40]
+opn = col_list[40:50]
+
+data_sums = pd.DataFrame()
+data_sums['extroversion'] = df_model[ext].sum(axis=1)/10
+data_sums['neurotic'] = df_model[est].sum(axis=1)/10
+data_sums['agreeable'] = df_model[agr].sum(axis=1)/10
+data_sums['conscientious'] = df_model[csn].sum(axis=1)/10
+data_sums['open'] = df_model[opn].sum(axis=1)/10
+data_sums['clusters'] = predictions
+data_sums.groupby('clusters').mean()
+
+# Visualizing the means for each cluster
+dataclusters = data_sums.groupby('clusters').mean()
+plt.figure(figsize=(22,3))
+for i in range(0, 5):
+    plt.subplot(1,5,i+1)
+    plt.bar(dataclusters.columns, dataclusters.iloc[:, i], color='green', alpha=0.2)
+    plt.plot(dataclusters.columns, dataclusters.iloc[:, i], color='red')
+    plt.title('Cluster ' + str(i))
+    plt.xticks(rotation=45)
+    plt.ylim(0,4);
+
+pca = PCA(n_components=2)
+pca_fit = pca.fit_transform(df_model)
+
+df_pca = pd.DataFrame(data=pca_fit, columns=['PCA1', 'PCA2'])
+df_pca['Clusters'] = predictions
+df_pca.head()
+plt.figure(figsize=(10,10))
+sns.scatterplot(data=df_pca, x='PCA1', y='PCA2', hue='Clusters', palette='Set2', alpha=0.8)
+plt.title('Personality Clusters after PCA');
+plt.savefig('clusters.png')
